718fe83fbdeba89ac4c47f469decc4bfe9068241
dblugeon <dblugeon@users.noreply.github.com>
2024-02-13 22:23:19 +0000

    * add set pipefail * add extentions download for vscode * add output with commit_ref version_ref * add download vscode server * add prepare vscode server

--- download_vscode.sh
+++ download_vscode.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
+set -o pipefail
 URL_VSCODE="https://code.visualstudio.com/sha/download?build=stable&os=win32-x64-archive"
-VS_UNCOMPRES_DIR=dist/vs_uncrompress
-rm -rf dist
-mkdir -p dist/extensions
+VS_UNCOMPRES_DIR=dist/vs_uncompress
 
+# extensions wanted list
+EXTENSIONS_ID="redhat.vscode-yaml redhat.ansible ms-vscode-remote.vscode-remote-extensionpack natizyskunk.sftp \
+johnstoncode.svn-scm eamodio.gitlens gitlab.gitlab-workflow"
+
+#clean up for testing 
+rm -r dist $HOME/.vscode-server &> /dev/null
+mkdir -p dist &> /dev/null
+
+echo "Download the latest version of vscode for windows..."
 (cd dist && curl $URL_VSCODE --location --remote-name --remote-header-name)
 RC_CURL=$?
 if [ $RC_CURL -ne 0 ]
@@ -11,7 +19,9 @@ then
     echo "[ERROR] during download vscode executable"
     exit $RC_CURL
 fi
-echo "extracting vscode"
+echo "[OK]"
+
+echo "Extracting vscode..."
 vscode_zip_name=$(ls dist/VSC*.zip)
 RC_LS=$?
 if [ $RC_LS -ne 0 ]
@@ -19,6 +29,9 @@ then
     echo "[ERROR] during search vscode's zip file"
     exit $RC_LS
 fi
+echo "[OK]"
+
+echo "Unzip vscode..."
 unzip $vscode_zip_name -d $VS_UNCOMPRES_DIR
 RC_UNZIP=$?
 if [ $RC_UNZIP -ne 0 ]
@@ -26,7 +39,9 @@ then
     echo "[ERROR] during unzip vscode"
     exit $RC_UNZIP
 fi
-echo "extract commit ref from $name's content"
+echo "[OK]"
+
+echo "Extract commit ref from $name's content..."
 commit_ref=$(cat $VS_UNCOMPRES_DIR/resources/app/product.json | jq -r '.commit')
 RC_COMMIT_REF=$?
 if [ $RC_COMMIT_REF -ne 0 ]
@@ -34,4 +49,58 @@ then
     echo "[ERROR] during extract commit ref of $name"
     exit $RC_UNZIP
 fi
-echo "download vscode server linux for $commit_ref"
+echo "commit_ref=$commit_ref" >> $GITHUB_OUTPUT    
+echo "[ok]"
+
+echo "Extract version tag $name's content..."
+version_ref=$(cat $VS_UNCOMPRES_DIR/resources/app/product.json | jq -r '.version')
+RC_VERSION_REF=$?
+if [ $RC_VERSION_REF -ne 0 ]
+then
+    echo "[ERROR] during extract commit ref of $name"
+    exit $RC_VERSION_REF
+fi
+echo "version_ref=$version_ref" >> $GITHUB_OUTPUT 
+echo "[ok]"
+
+# source https://stackoverflow.com/questions/56718453/using-remote-ssh-in-vscode-on-a-target-machine-that-only-allows-inbound-ssh-co
+echo "Download vscode server linux for commit $commit_ref..."
+(cd dist && curl --location --remote-name --remote-header-name "https://update.code.visualstudio.com/commit:${commit_ref}/server-linux-x64/stable" )
+RC_SERVER_LINUX=$?
+if [ $RC_SERVER_LINUX -ne 0 ]
+then
+    echo "[ERROR] fail during download linux vscode server"
+    exit $RC_SERVER_LINUX=$?
+fi
+echo "[ok]"
+
+echo "Untar vscode server..."
+( cd dist && tar -xvzf vscode-server-linux-x64.tar.gz )
+RC_UNTAR=$?
+if [ $RC_UNTAR -ne 0 ]
+then
+    echo "[ERROR] fail during untar linux vscode server"
+    exit $RC_UNTAR=$?
+fi
+echo "[ok]"
+
+echo "Installing extension in vscode server"
+for ext in $EXTENSIONS_ID
+do
+    dist/vscode-server-linux-x64/bin/code-server --install-extension $ext
+done
+echo "[OK]"
+
+echo "Prepare vscode final version"
+mv $HOME/.vscode-server dist/vscode-server && \
+mkdir dist/vscode-server/bin && \
+mv dist/vscode-server-linux-x64 dist/vscode-server/bin/${commit_ref} && \
+rm dist/vscode-server-linux-x64.tar.gz
+
+if [ $? -ne 0 ]
+then
+    echo "[ERROR] during preparation first stage"
+    exit -1
+fi
+
+echo "[ok]"
